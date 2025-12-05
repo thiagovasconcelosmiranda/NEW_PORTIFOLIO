44baaf7385e0efeb5ecd5963d5284842386f3457
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2025-12-05 18:21:03 -0300

    Nova atualização

--- public/assets/js/link-scroll-script.js
+++ public/assets/js/link-scroll-script.js
@@ -1,24 +1,56 @@
+
 (function () {
     window.addEventListener('load', () => {
         const scroll = new Scroll;
 
-        document.querySelectorAll('.link a').forEach(link => {
-           
+        document.querySelectorAll('ul .link a').forEach(link => {
+
             link.addEventListener('click', (e) => {
                 const activeAll = document.querySelector('.active');
 
-                if(activeAll){
-                   activeAll.classList.remove('active');
+                if (activeAll) {
+                    activeAll.classList.remove('active');
                 }
-               
+
                 e.target.classList.add('active');
-                e.preventDefault(link);  
+                e.preventDefault(link);
                 scroll.scrollToIdOnclick(link);
             });
         });
 
-        window.addEventListener('scroll', () => {
-         
-        })
-    });
+          
+
+           
+
+            document.querySelectorAll('ul .link a').forEach(item => {
+                const href = item.href.split('#')[1];
+                console.log(item)
+                if(href)
+
+                    observeItem(href)
+                
+            });
+
+            function observeItem (href) {
+               
+               const section = document.querySelector(`#${href}`);
+               const dataLink = document.querySelector(`.${href}`)
+               console.log(section)
+               const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        dataLink.classList.add('active');
+                    } else {
+                        dataLink.classList.remove('active');
+                    }
+                });
+            }, { threshold: 0.5 });
+            observer.observe(section);
+            
+        
+            }
+
+           
+
+        });
 }());
