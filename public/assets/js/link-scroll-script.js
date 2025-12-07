30aad9e719b78501b5137299c84154868d3f0c66
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2025-12-07 11:29:52 -0300

    Nova atualização

--- public/assets/js/link-scroll-script.js
+++ public/assets/js/link-scroll-script.js
@@ -2,7 +2,7 @@
 (function () {
     window.addEventListener('load', () => {
         const scroll = new Scroll;
-        const links = document.querySelectorAll('ul .link a');
+        const links = document.querySelectorAll('.link a');
 
         links.forEach(link => {
 
@@ -14,7 +14,8 @@
                 }
 
                 e.target.classList.add('active');
-                e.preventDefault(link);
+                e.preventDefault();
+
                 scroll.scrollToIdOnclick(link);
             });
         });
@@ -30,7 +31,7 @@
 
             const section = document.querySelector(`#${href}`);
             const dataLink = document.querySelector(`.${href}`)
-            
+
             const observer = new IntersectionObserver((entries) => {
                 entries.forEach(entry => {
                     if (entry.isIntersecting) {
@@ -41,10 +42,33 @@
                 });
             }, { threshold: 0.5 });
             observer.observe(section);
+            let count = 1;
+            document.querySelectorAll('.user-info-cards .card-item span').forEach(item => {
+                count++;
+                item.id = count;
 
+                window.addEventListener('scroll', () => {
+                    const position = window.scrollY;
 
+                    if (position > 805.5556030273438)
+                        countAnimationNumber(`#${item.id}`, parseInt(item.getAttribute('data-qtd')))
+                });
+
+            });
         }
 
+
+        function countAnimationNumber(element, number) {
+            $(element).prop('counter', 0).animate({
+                counter: number
+            }, {
+                duration: 1000, // Duração da animação em milissegundos (1 segundo)
+                easing: 'swing', // Tipo de suavização
+                step: function (agora) { // 'agora' é o valor atual da animação
+                    $(this).text(Math.ceil(agora)); // Arredonda para cima e atualiza o texto
+                }
+            });
+        }
 
 
     });
