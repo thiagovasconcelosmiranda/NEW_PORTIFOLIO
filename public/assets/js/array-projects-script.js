7119713667d7f5622152a60624e82f83704c3241
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2025-12-12 20:06:41 -0300

    Nova atualiação

--- public/assets/js/array-projects-script.js
+++ public/assets/js/array-projects-script.js
@@ -21,20 +21,20 @@ let projects = [
         image: '/assets/images/projects/cosmeticos.png',
         link: '',
         style: '',
-        title: 'Site Web',
+        title: 'Loja Virtual Saúde cosméticos',
         category: 'site',
         classBackground: 'background-rose',
         github: ''
     },
     {
         id: 2,
-        description: " Projeto para divugação de óculos de graus e sol. O sate conta com um designer moderno e leve para o usuúrio navegar.",
+        description:"Projeto para divugação de óculos de graus e sol. O sate conta com um designer moderno e leve para o usuúrio navegar.",
         linguas: ['fa-brands/fa-html5', 'fa-brands/fa-css3-alt', 'fa-brands/fa-sass', 'fa-brands/fa-node', 'fa-brands/fa-git', 'fa-solid/fa-database'],
         video: '',
         image: '/assets/images/projects/otica.png',
         link: 'https://otica-node.vercel.app/',
         style: '',
-        title: 'Site Web',
+        title: 'Site Web Ótica',
         category: 'site',
         classBackground: 'background-blue',
         github: 'https://github.com/thiagovasconcelosmiranda/otica_node'
@@ -47,7 +47,7 @@ let projects = [
         image: '/assets/images/projects/owflow.png',
         link: 'https://onflow-go-cod.vercel.app/',
         style: '',
-        title: 'Site Web',
+        title: 'Site OwflowGo',
         category: 'site',
         classBackground: 'background-rose',
         github: 'https://github.com/thiagovasconcelosmiranda/OnflowGo-Cod'
@@ -61,7 +61,7 @@ let projects = [
         image: '/assets/images/projects/icon.png',
         link: 'https://icom-bqcn.vercel.app/',
         style: '',
-        title: 'Site web',
+        title: 'Projeto web ICON Libras',
         category: 'site',
         classBackground: 'background-blue',
         github: 'https://github.com/thiagovasconcelosmiranda/Icon-libras'
