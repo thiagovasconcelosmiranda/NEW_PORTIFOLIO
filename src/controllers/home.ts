a7c8cffe0e77a6716f5863ea14656be7443af429
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2026-05-07 10:02:00 -0300

    Nova  Atualização
    Correção na função calcBirth() homeController.ts

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from "express";
-import {getDate} from '../utils/date';
+import { getDate } from '../utils/date';
 
 export const home = (req: Request, res: Response) => {
    const viewHeader: boolean = true;
    const viewFooter: boolean = true;
    const year: any = getDate()[2];
-   
-   
+   const birth = calcBirth('1984-05-06');
+
    res.render('home', {
-      viewHeader, viewFooter, year
+      viewHeader, viewFooter, year, birth
    });
+}
+
+const calcBirth = (birth: string) => {
+
+   const today = new Date();
+
+   const dateBirth = new Date(birth);
+
+   let age = today.getFullYear() - dateBirth.getFullYear();
+   const month = today.getMonth() - dateBirth.getMonth();
+
+   if (month < 0 || (month === 0 && today.getDate() <= dateBirth.getDate())) {
+      age--;
+   }
+   return age;
+
 }
