4f961b3e696f228c70f2a69bc7102ebf340fc59b
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2026-06-13 11:38:04 -0300

    Nova atualização
    Ajuste na galeria de projetos realizados

--- public/assets/js/array-projects-script.js
+++ public/assets/js/array-projects-script.js
@@ -1,33 +1,9 @@
 
 let projects = [
+   
+   
     {
         id: 0,
-        description: "Projeto em desenvolvimento! Breve estará disponível.",
-        linguas: ['fa-brands/fa-react', 'fa-brands/fa-css3-alt', 'fa-brands/fa-git', 'fa-solid/fa-database'],
-        video: '',
-        image: '/assets/images/projects/mobile2.jpg',
-        link: '',
-        style: 'image-grid-col-2 image-grid-row-2',
-        title: 'Em desenvolvimento',
-        category: 'app',
-        classBackground: 'rose',
-        github: ''
-    },
-    {
-        id: 1,
-        description: "Site ecommerce, com foco em vendas de cosméticos (Avon, Boticário e muito mais). Projeto conta com busca avançada de produto, proporcionando uma busca rápida para o usuário. Conta com sistema de authenticação, compras online, carrinho de compras, meus favoritos, e opção de pagamentos online (pix, boleto e cartão de credito), calculo de frete em tempo real.",
-        linguas: ['fa-brands/fa-html5', 'fa-brands/fa-css3-alt', 'fa-brands/fa-php', 'fa-brands/fa-git', 'fa-solid/fa-database'],
-        video: 'cosmeticos.mp4',
-        image: '/assets/images/projects/cosmeticos.png',
-        link: '',
-        style: '',
-        title: 'Loja Virtual Saúde Cosméticos',
-        category: 'site',
-        classBackground: 'background-rose',
-        github: ''
-    },
-    {
-        id: 2,
         description:"Projeto para divugação de óculos de graus e sol. O sate conta com um designer moderno e leve para o usuúrio navegar.",
         linguas: ['fa-brands/fa-html5', 'fa-brands/fa-css3-alt', 'fa-brands/fa-sass', 'fa-brands/fa-node', 'fa-brands/fa-git', 'fa-solid/fa-database'],
         video: '',
@@ -40,7 +16,7 @@ let projects = [
         github: 'https://github.com/thiagovasconcelosmiranda/otica_node'
     },
     {
-        id: 3,
+        id: 1,
         description: "Projeto desenvolvido para venda e divugação de receptores de satéites e IPTV. No site você encontra driver para atualização de equipamentos e vendas de pacotes.",
         linguas: ['fa-brands/fa-html5', 'fa-brands/fa-css3-alt', 'fa-brands/fa-sass', 'fa-brands/fa-node', 'fa-brands/fa-git', 'fa-solid/fa-database'],
         video: '',
@@ -54,33 +30,20 @@ let projects = [
 
     },
     {
-        id: 4,
+        id: 2,
         description: "Projeto desenvolvido pegando de modelo o site expirado no designer <a target='__blank' href='https://www.icom.app/'>App libras</a>. Desenvolvido para aperfeiçoamento no estudos da tecnologia nodeJS.",
         linguas: ['fa-brands/fa-html5', 'fa-brands/fa-css3-alt', 'fa-brands/fa-php', 'fa-brands/fa-git', 'fa-solid/fa-database'],
         video: '',
         image: '/assets/images/projects/icon.png',
         link: 'https://icom-bqcn.vercel.app/',
         style: '',
-        title: 'Projeto web ICON Libras',
+        title: 'Projeto Web ICON Libras',
         category: 'site',
         classBackground: 'background-blue',
         github: 'https://github.com/thiagovasconcelosmiranda/Icon-libras'
     },
     {
-        id: 5,
-        description: "Projeto em desenvolvimento! Breve estará disponível.",
-        linguas: ['fa-brands/fa-react', 'fa-brands/fa-css3-alt', 'fa-brands/fa-git', 'fa-solid/fa-database'],
-        video: '',
-        image: '/assets/images/projects/mobile2.jpg',
-        link: '',
-        style: 'image-grid-col-2 image-grid-row-2',
-        title: 'Em desenvolvimento',
-        category: 'app',
-        background: '#542a52',
-        github: ''
-    },
-    {
-        id: 6,
+        id: 3,
         description: "Loja virtual ecommerce. Vendas de games e jogos.",
         linguas: ['fa-brands/fa-html5', 'fa-brands/fa-css3-alt', 'fa-brands/fa-php', 'fa-brands/fa-git', 'fa-solid/fa-database'],
         video: 'dc_games.mp4',
